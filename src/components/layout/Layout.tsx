703d1ef9afb7baf47841844b6c0bffdd0fd6a63c
Narendra <personal.rendra@gmail.com>
2023-03-24 09:17:05 +0700

    [ADD] Login page
    Login page design mockup
    
    Author: Narendra

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Layout = ({children}: any) => {
+  return (
+    <div>Layout</div>
+  )
+}
+
+export default Layout
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+import { BrowserRouter as Router } from "react-router-dom";
 
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(
-//   document.getElementById('root') as HTMLElement
-// );
+// Custom chakra theme
+const theme = extendTheme({
+  colors: {
+    primary: {
+      100: '#F9F7F7',
+      200: '#DBE2EF',
+      300: '#3F72AF',
+      400: '#112D4E',
+    },
+    secondary: {
+      100: '#F2E9D0',
+      200: '#EACEB4',
+      300: '#E79E85',
+      400: '#BB5A5A',
+    }
+  },
+  styles: {
+    global: {
+      body: {
+        bg: '#FFFDFA',
+        color: '#343434',
+      }
+    }
+  }
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <Router>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -22,4 +49,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
