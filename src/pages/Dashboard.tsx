7d4b08c967e81a843d069a51a36452e0cbe10683
Narendra <personal.rendra@gmail.com>
2023-03-26 21:29:59 +0700

    [UPD] Dashboard text

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Center, Text } from '@chakra-ui/layout'
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Ki Seratus App</h1>
-      <p>Dashboard Page</p>
-    </div>
+    <Center className='dashboard-page' height='calc(100vh - 112px)' color={'gray.400'} flexDirection={'column'}>
+      <Text fontSize={{md: '3xl', base: 'xl'}}>Selamat datang di</Text>
+      <Text fontSize={{md: "5xl", base: '3xl'}} align={'center'}>Aplikasi KiSeratus</Text>
+    </Center>
   )
 }
 
